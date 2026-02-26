f10c70b0a11af3777b8ad7de07dc6942b491325d
VictorPeso <victor.peso@gmail.com>
2026-02-26 17:07:27 +0100

    0.1.0 - Funcion mean anomaly crseada falta corregir

---
+++ mean_anomaly.m
@@ -0,0 +1,24 @@
+% Compute the Mean Anomaly at "dt" seconds after ToA, given the Mean Anomaly at ToA and 
+% the semi-major axis 
+% G = gravitational constant - 5 -  
+% Me = Earth mass 
+% a = semi-major axis [m] 
+% Mo = Mean Anomaly at ToA [rad] 
+% dt = seconds elapsed after ToA (negative if ToA is in the future) 
+% M = Mean Anomaly at ToA + dt [rad] 
+function M = mean_anomaly(G, Me, a, Mo, dt) 
+ % ===== your code goes here ====
+    % Earth gravitational parameter
+    mu = G * Me; % [m^3/s^2]
+
+    % Mean motion (rad/s)
+    n = sqrt(mu / (a^3));
+
+    % Propagate mean anomaly
+    M = Mo + n * dt;
+    
+    if ismember("fd", who("global")) 
+        global fd; 
+        fprintf(fd, "dt = %f, M = %f\n", dt, mod(M, 2 * pi)); 
+    end 
+end 
